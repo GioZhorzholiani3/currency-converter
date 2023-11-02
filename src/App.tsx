bba90c72700e7f80e83b8a5806e861cd0ef7f402
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-11-02 14:07:23 +0400

    add inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import CurrencyRow from "./components/CurrencyRow";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div>=</div>
+      <CurrencyRow />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/CurrencyRow.tsx
@@ -0,0 +1,12 @@
+const CurrencyRow = () => {
+  return (
+    <div>
+      <input type="number" />
+      <select>
+        <option value="hi">hi</option>
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyRow;
